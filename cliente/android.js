487dd8baa5d024b54cd39e9d51231c4286524345
leonardobussi <leonardobezerrabussy@gmail.com>
2020-07-30 17:58:16 -0300

    init

--- cliente/android.js
+++ cliente/android.js
@@ -1,26 +1,34 @@
-var http = require('http')
+var http = require('http');
 
 var opcoes = {
-    hostname: 'localhost',
-    port: 80,
-    path: '/',
-    headers: {
-        'Accept': 'application/json'
-    }
+	hostname: 'localhost',
+	port: 80,
+	path: '/',
+	method: 'post',
+	headers: {
+		'Accept' : 'application/json',
+		'Content-type' : 'application/json'
+	}
 }
 
-var buffer_corpo_responde = []
+//Content-type
+var html = 'nome=José'; //x-www-form-urlencoded
+var json = {nome : 'José'};
+var string_json = JSON.stringify(json);
 
-http.get(opcoes, function(res){
-    res.on('data', function(pedaco){
-        buffer_corpo_responde.push(pedaco)
-    })
-    res.on('end', function(){
-        var corpo = Buffer.concat(buffer_corpo_responde).toString()
-        console.log(corpo)
-    })
+var buffer_corpo_response = [];
 
-    res.on('error', function(){
+var req = http.request(opcoes, function(res){
 
-    })
-})
+	res.on('data', function(pedaco){
+		buffer_corpo_response.push(pedaco);
+	});
+
+	res.on('end', function(){
+		var corpo_responde = Buffer.concat(buffer_corpo_response).toString();
+		console.log(corpo_responde);
+	});
+});
+
+req.write(string_json);
+req.end();
